10ac9f150bc6663356488433281adf008864a5f7
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-18 23:49:07 +0100

    Add VerovioCanvas component

---
+++ src/components/VerovioCanvas.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  width: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+});
+
+const style = ref({
+    width: props.width === 0 ? '100%'  : `${parseInt(props.width, 10)}px`,
+    backgroundColor: 'grey',
+});
+
+</script>
+
+<template>
+  <div class="verovio-canvas" :style="style">
+    <pre>{{ style }}</pre>
+  </div>
+</template>
