78c836fede9cc83984cc9aff6b03c003c754b3f6
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-20 21:49:52 +0100

    Revert "Remove @vue/eslint-config-prettier"
    This reverts commit 46fb52e6d5d64942836cec506de0fc1b8f34df04.
    
    # Conflicts:
    #	.eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     extends: [
         'plugin:vue/vue3-essential',
         'eslint:recommended',
+        '@vue/eslint-config-prettier'
     ],
     env: {
         'vue/setup-compiler-macros': true,
