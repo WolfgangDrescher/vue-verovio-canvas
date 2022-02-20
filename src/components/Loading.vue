2d5bf3ebdfd1885f68c0bac2d6f3de40af60e563
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-20 17:12:01 +0100

    Add Loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,50 @@
+<script setup>
+import LoadingSpinner from './LoadingSpinner.vue';
+
+const props = defineProps({
+    message: {
+        type: String,
+        default: 'Loading…',
+    },
+    showMessage: {
+        type: Boolean,
+        default: true,
+    },
+    spinnerSize: {
+        type: Number,
+        default: 64,
+    },
+    spinnerColor: {
+        type: String,
+        default: '#000',
+    },
+});
+</script>
+
+<template>
+    <div class="loading-component">
+        <div class="loading-component-inner">
+            <LoadingSpinner :size="spinnerSize" :stroke="spinnerColor" />
+            <div v-if="showMessage" class="loading-component-message">{{ message }}</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.loading-component {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.loading-component-inner {
+    text-align: center;
+}
+
+.loading-component-message {
+    text-align: center;
+    margin-top: .5rem;
+}
+</style>
