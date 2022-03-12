a89ed52a5b3aea12c9e3b9b4eabcf0a9689ee532
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-12 13:39:51 +0100

    Improve demo content in app component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,59 @@
 <script setup>
 import VerovioCanvas from './components/VerovioCanvas.vue';
+
+const kernScore = `**kern	**kern	**kern
+*staff3	*staff2	*staff1
+*clefF4	*clefG2	*clefG2
+*k[b-e-]	*k[b-e-]	*k[b-e-]
+*g	*g	*g
+*M3/8	*M3/8	*M3/8
+=1	=1	=1
+4G	4.r	16r
+.	.	16ddL
+.	.	16b-
+.	.	16g
+8E-	.	8ggJ
+=2	=2	=2
+4D	16r	4.ff
+.	16aL	.
+.	16f	.
+.	16d	.
+8B-	[8ddJ	.
+=3	=3	=3
+4G	8.dd]L	4.ee-
+.	16b-	.
+8A	[8ccJ	.
+=4	=4	=4
+16B-L	8.cc]L	4.dd
+16d	.	.
+16B-	.	.
+16G	16a	.
+[8gJ	8b-J	.
+=5	=5	=5
+8.g]L	4.a	4cc
+16e	.	.
+8f#J	.	8dd
+=6	=6	=6
+4g	16aL	8.b-L
+.	16f#J	.
+.	[4g	.
+.	.	16b-
+8c	.	8ee-J
+=7	=7	=7
+4d	4g]	8.aL
+.	.	16b-
+8D	8f#	8ccJ
+=8	=8	=8
+16GL	4.g	4.b-
+16D	.	.
+16BB-	.	.
+16GG	.	.
+8GJ	.	.
+*-	*-	*-
+`;
 </script>
 
 <template>
-    <VerovioCanvas />
-    <VerovioCanvas :width="500" />
+    <VerovioCanvas url="https://raw.githubusercontent.com/WolfgangDrescher/lassus-geistliche-psalmen/master/kern/01-beatus-vir.krn" />
+    <VerovioCanvas :data="kernScore" />
 </template>
