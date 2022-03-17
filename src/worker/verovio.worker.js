825eeedf8b455921911308bb5abc72b738cf9d24
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-17 13:10:14 +0100

    Add verovio.worker.js

---
+++ src/worker/verovio.worker.js
@@ -0,0 +1,44 @@
+import verovio from 'verovio-humdrum';
+import { useDeferred } from '../composables/useDeferred';
+
+if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
+
+    let verovioToolkit = {};
+
+    const moduleIsReady = useDeferred();
+
+    verovio.module.onRuntimeInitialized = function () {
+        verovioToolkit = new verovio.toolkit();
+        moduleIsReady.resolve();
+    };
+
+    addEventListener('message', function (event) {
+        console.log('worker', 'message', event);
+        const { id, method, args } = event.data;
+
+        if(method === 'onRuntimeInitialized') {
+            moduleIsReady.promise.then(() => {
+                postMessage({
+                    id,
+                    method,
+                    args,
+                    result: null,
+                }, event);
+            });
+            return;
+        }
+
+        const fn = verovioToolkit[method || null];
+        let result;
+        if(fn) {
+            result = fn.apply(verovioToolkit, args || []);
+        }
+
+        postMessage({
+            id,
+            method,
+            args,
+            result,
+        }, event);
+    });
+}
