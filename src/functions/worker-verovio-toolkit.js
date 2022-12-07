1d8b2129e52ea59cbf339eb7a979f1680b873910
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-12-07 23:36:08 +0100

    Add WorkerVerovioToolkit proxy

---
+++ src/functions/worker-verovio-toolkit.js
@@ -0,0 +1,29 @@
+import { Deferred } from '../classes/deferred.js';
+import { v4 as uuidv4 } from 'uuid';
+
+export function createWorkerVerovioToolkit(worker) {
+    const tasks = {};
+    worker.addEventListener('message', (event) => {
+        const { id, result } = event.data
+        const deferred = tasks[id];
+        if (deferred) {
+            deferred.resolve(result);
+            delete tasks[id];
+        }
+    });
+    return new Proxy({}, {
+        get: (target, method) => {
+            return (...args) => {
+                const id = uuidv4();
+                worker.postMessage({
+                    id,
+                    method,
+                    args,
+                });
+                const deferred = new Deferred();
+                tasks[id] = deferred;
+                return deferred.promise;
+            };
+        },
+    });
+}
