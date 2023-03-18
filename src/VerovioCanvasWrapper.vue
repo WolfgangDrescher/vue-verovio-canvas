3bd9959df8f2d0cdb677978f9b5bdadc8a6f5b20
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-18 18:02:43 +0100

    Listen to load and unload events

--- src/VerovioCanvasWrapper.vue
+++ src/VerovioCanvasWrapper.vue
@@ -1,13 +1,25 @@
 <script setup>
+import { ref } from 'vue';
 import { VerovioCanvas, createWorkerVerovioToolkit } from './index.js';
 
 const props = defineProps({
     worker: Object,
 });
 
-const toolkit = createWorkerVerovioToolkit(props.worker);
+const toolkit = ref();
+
+function loadToolkit() {
+    toolkit.value = createWorkerVerovioToolkit(props.worker);
+}
+
+function unloadToolkit() {
+    if (toolkit.value) {
+        toolkit.value.removeToolkit();
+        toolkit.value = null;
+    }
+}
 </script>
 
 <template>
-    <VerovioCanvas :toolkit="toolkit" />
+    <VerovioCanvas :toolkit="toolkit" @load="loadToolkit" @unload="unloadToolkit" />
 </template>
