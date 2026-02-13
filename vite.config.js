89cd30b4fd68ba9d51a453399d9b92a712a2d0dd
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-02-13 15:04:53 +0100

    Remove deprecated splitVendorChunkPlugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), splitVendorChunkPlugin()],
+    plugins: [vue()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
